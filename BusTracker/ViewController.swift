c1a8ed42e1e6e4615c8efce8d2827e1f8c1cabb6
botichelli <regmesmal@gmail.com>
2020-06-11 20:51:18 +0200

    Initial Commit

---
+++ BusTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusTracker
+//
+//  Created by botichelli on 6/11/20.
+//  Copyright © 2020 Oleksandra Sildushkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
